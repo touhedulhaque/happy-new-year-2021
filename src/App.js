4317314721bd17f90921c8ad0fdc236dee5b16d0
Md Touhedul Haque <razmbamkt@gmail.com>
2021-01-01 09:48:00 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js';
+import logo from '../src/img/happy-new-year.jpg';
+import logo2 from '../src/img/2020-Happy-New-Year-gifs.gif'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div  >
+        <Particles
+          params={{
+            particles: {
+              line_linked: {
+                shadow: {
+                  enable: true,
+                  color: "#8a2be2",
+                  blur: 5
+                }
+              }
+            }
+          }}
+          style={{
+            width: '100%',
+            backgroundImage: `url(${logo})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat'
+          }}
+        />
+        <div className="container text-center">
+          <h1>"Wish you all the best and a Good start from 2021" </h1>
+          <p className="text-muted ml-auto">Greetings from <span className="font-weight-bolder text-primary">Haque(RAZz)</span> </p>
+          <img src={logo2} className="img-fluid"></img>
+
+        </div>
+
+      </div>
     </div>
   );
 }
